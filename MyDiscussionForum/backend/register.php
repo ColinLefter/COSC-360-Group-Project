f87132cd0a0c7061ab2e6bed7df5fd767b5b79be
colinlefter <colin.lefter02@gmail.com>
2024-03-28 13:11:31 -0700

    Attempted fix for register.php

--- MyDiscussionForum/backend/register.php
+++ MyDiscussionForum/backend/register.php
@@ -5,7 +5,7 @@ include "databaseFunc.php";
 include "validation.php";
 include "commonFunctions.php";
 
-session_start();
+// session_start();
 
 if (!validateMethodPost()) {
     exit(); // Stop script execution if not POST
@@ -34,7 +34,7 @@ $passhash = password_hash($password, PASSWORD_DEFAULT);
 $connection = connectToDB();
 
 // Prepared statement for user existence check
-$stmt = $connection -> prepare("SELECT username, email FROM user WHERE username = ? OR email = ?");
+$stmt = $connection -> prepare("SELECT userName, email FROM user WHERE userName = ? OR email = ?");
 $stmt -> bind_param("ss", $username, $email);
 $stmt -> execute();
 $result = $stmt -> get_result();
@@ -44,11 +44,13 @@ if ($result -> num_rows > 0) {
     exit();
 } else {
     // Prepared statement for inserting user information
-    $insertStmt = $connection -> prepare("INSERT INTO user (userName, firstName, lastName, email, accountAge, password) VALUES (?, ?, ?, ?, ?)");
+    $insertStmt = $connection -> prepare("INSERT INTO user (userName, firstName, lastName, email, accountAge, password) VALUES (?, ?, ?, ?, ?, ?)");
     $insertStmt -> bind_param("ssssss", $username, $firstname, $lastname, $email, "30 days", $passhash); // For now every account age is 30 days. We will change this later.
 
     if ($insertStmt -> execute()) {
-        trackUserActivity($connection, $_SESSION['userId'], "ACCOUNT_CREATED");
+        // We have to get the last created user's ID with the following approach:
+        $lastInsertedId = $connection -> insert_id; 
+        trackUserActivity($connection, $lastInsertedId, "ACCOUNT_CREATED");
         returnData("ACCOUNT_CREATION_SUCCESS", $connection);
     } else {
         // Handle potential errors in account creation
