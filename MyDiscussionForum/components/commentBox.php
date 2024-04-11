ca41126a4e4aef76a0481921d238368e754fc613
colinlefter <colin.lefter02@gmail.com>
2024-04-11 16:37:58 -0700

    Added further dark theme support for comment box

--- MyDiscussionForum/components/commentBox.php
+++ MyDiscussionForum/components/commentBox.php
@@ -22,11 +22,11 @@
               <textarea id='comment-text-area' name='comment-content' rows='5'></textarea>
             </div>
             <div>
-              <input type='submit' id='comment-submit-button' class='comment-button' value='Comment'>
-              <input type='button' id='comment-discard-button' class='comment-button' value='Discard'>
+              <input type='submit' id='comment-submit-button' class='button-base primary-button' value='Comment'>
+              <input type='button' id='comment-discard-button' class='button-base primary-button' value='Discard'>
             </div>
           </form>
-      </section>
+    </section>
 
   <script src="js/functions.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
